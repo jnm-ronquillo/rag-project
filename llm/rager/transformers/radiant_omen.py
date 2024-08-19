a8fdff15bca699a1dfb1785d19b971f2806edc18
Juan Manuel Rojas Ronquillo <55019923+jnm-ronquillo@users.noreply.github.com>
2024-08-19 03:00:46 +0000

    hw5

---
+++ llm/rager/transformers/radiant_omen.py
@@ -0,0 +1,37 @@
+import hashlib
+
+if 'transformer' not in globals():
+    from mage_ai.data_preparation.decorators import transformer
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@transformer
+def transform(data, *args, **kwargs):
+    documents = []
+    print(type(data))
+    for doc in data['documents']:
+        doc['course'] = data['course']
+        # previously we used just "id" for document ID
+        doc['document_id'] = generate_document_id(doc)
+        documents.append(doc)
+
+    print(len(documents))
+
+    return documents
+
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
+
+def generate_document_id(doc):
+    combined = f"{doc['course']}-{doc['question']}-{doc['text'][:10]}"
+    hash_object = hashlib.md5(combined.encode())
+    hash_hex = hash_object.hexdigest()
+    document_id = hash_hex[:8]
+    return document_id
